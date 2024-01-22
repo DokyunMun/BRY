01f44da72b66fb055084c2a92cdf231be53f8333
dokyunmun <superdokyun@gmail.com>
2024-01-23 01:26:39 +0900

    0123-4

--- my-app/past/artworkserver-mob.js
+++
@@ -1,9 +0,0 @@
-// my-app/app/ArtworksServer.tsx
-import DataFetcher from "./datafetcher";
-import Artworks from "./artworks-mob";
-
-export default async function ArtworksServermob() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/datafetcher`);
-  const artworks = await res.json();
-  return <Artworks artworks={artworks} />;
-}
--- my-app/past/artworkserver.js
+++
@@ -1,8 +0,0 @@
-// my-app/app/ArtworksServer.tsx
-import Artworks from "../app/artworks";
-
-export default async function ArtworksServer() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/datafetcher`);
-  const artworks = await res.json();
-  return <Artworks artworks={artworks} />;
-}
