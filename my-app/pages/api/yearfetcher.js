5d52145563eaa041736f5268cf82102c9a40ec44
dokyunmun <superdokyun@gmail.com>
2024-03-08 01:56:47 -0500

    Update yearfetcher.js

--- my-app/pages/api/yearfetcher.js
+++ my-app/pages/api/yearfetcher.js
@@ -4,7 +4,10 @@ export default async function handler(req, res) {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
-  const { data: artworks } = await supabase.from("artworks").select("year");
+  const { data: artworks } = await supabase
+    .from("artworks")
+    .select("year")
+    .order("year", { ascending: true });
 
   res.status(200).json(artworks); // 결과를 JSON 형식으로 반환합니다.
 }
