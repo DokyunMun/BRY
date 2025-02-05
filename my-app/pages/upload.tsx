ef85a4e4eed0824d7217098b8f1235442bf29439
. <85930592+DokyunMun@users.noreply.github.com>
2025-02-05 17:30:00 +0900

    new05

--- my-app/pages/upload.tsx
+++ my-app/pages/upload.tsx
@@ -2,7 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 import React, { useState, useEffect } from "react";
-import "./global.css";
+// import "./global.css";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
